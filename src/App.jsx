daa67159012e181de1caf11c545e93d58263528e
sunandasamanta <samantasunanda724@gmail.com>
2024-11-27 23:58:29 +0530

    initialize React project with Vite, Tailwind CSS, and i18next for internationalization

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { useTranslation } from 'react-i18next';
+
+function MyComponent () {
+  const { t, i18n } = useTranslation();
+  return <h1>{t('Welcome to React')}</h1>
+}
+
+export default MyComponent;
